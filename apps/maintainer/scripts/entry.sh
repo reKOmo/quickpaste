92795a55fc2df59938169e54bfc477322686d805
koMOre <zadanka.i.testy@protonmail.com>
2022-09-13 23:50:43 +0200

    fix cron 2

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting crontab"
-crond start -f -l 8 && echo "Started crond"
+crond start -l 8 && echo "Started crond"
 echo "Setting up certbot"
 certbot certonly --nginx --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
