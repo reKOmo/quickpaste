289086734c3b1f1ad0e964dfe3271ea8721a18df
koMOre <zadanka.i.testy@protonmail.com>
2022-09-14 01:39:47 +0200

    fix path

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -4,4 +4,4 @@ echo "Starting crontab"
 crond start -l 8 && echo "Started crond"
 echo "Setting up certbot"
 sleep 20
-certbot certonly --webroot --webroot-path=/usr/share/certbot --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
+certbot certonly --webroot --webroot-path /usr/share/certbot --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
