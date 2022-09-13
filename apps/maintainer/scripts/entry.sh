96c02a273f69a27ee15f6aed6ac62ec0ddd26880
koMOre <zadanka.i.testy@protonmail.com>
2022-09-14 01:10:30 +0200

    fix certbot nginx timing

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -3,4 +3,5 @@
 echo "Starting crontab"
 crond start -l 8 && echo "Started crond"
 echo "Setting up certbot"
-certbot certonly --standalone --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
+sleep 20
+certbot certonly --nginx --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
