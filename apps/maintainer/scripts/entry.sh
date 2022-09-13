6e3263b8cc7949ffa293e93242fae73e7542b5bb
koMOre <zadanka.i.testy@protonmail.com>
2022-09-13 22:20:33 +0200

    fix cron job blocking execution

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "starting crontab"
-crond -f -l 8
-echo "setting up certbot"
+echo "Starting crontab"
+crond start -f -l 8 && echo "Started crond"
+echo "Setting up certbot"
 certbot certonly --nginx --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
