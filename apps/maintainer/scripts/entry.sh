1db4b1bf4ff57fd9718deb09a56e6f58a23960b7
koMOre <zadanka.i.testy@protonmail.com>
2022-09-15 19:22:56 +0200

    no www

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -2,6 +2,7 @@
 
 echo "Starting crontab"
 crond start -l 8 && echo "Started crond"
+echo "Waiting 30s..."
+sleep 30
 echo "Setting up certbot"
-sleep 20
-certbot certonly --webroot --webroot-path /usr/share/certbot --agree-tos -d $DOMAIN_NAME -d www.$DOMAIN_NAME -n -m $EMAIL
+certbot certonly --webroot --webroot-path /usr/share/certbot --agree-tos -d $DOMAIN_NAME -n -m $EMAIL
