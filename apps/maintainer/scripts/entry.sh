2e795bf02b6abffa7a491883d9cd04eb95c1dfb4
reKOmo <40007531+reKOmo@users.noreply.github.com>
2022-12-20 16:39:00 +0100

    Keep maintainer alive after entrypoint

--- apps/maintainer/scripts/entry.sh
+++ apps/maintainer/scripts/entry.sh
@@ -6,3 +6,5 @@ echo "Waiting 30s..."
 sleep 30
 echo "Setting up certbot"
 certbot certonly --webroot --webroot-path /usr/share/certbot --agree-tos -d $DOMAIN_NAME -n -m $EMAIL
+#keep container alive
+tail -f /dev/null
