b4e53a3a94bd75beaaf037bcd49f9bf6d03efedd
rekomo <rekomo.code@protonmail.com>
2023-06-20 18:43:07 +0200

    fixed dev setup

--- apps/web/src/app.vue
+++ apps/web/src/app.vue
@@ -3,27 +3,28 @@
         <Title>Quickpaste</Title>
     </Head>
     <div class="bg-bg min-h-screen flex flex-col">
-        <MyNavigation/>
-        <NotificationsManager/>
+        <MyNavigation />
+        <NotificationsManager />
         <NuxtLayout name="default">
-            <NuxtPage/>
+            <NuxtPage />
         </NuxtLayout>
-        <Footer class="mt-auto"/>
+        <Footer class="mt-auto" />
     </div>
 </template>
 
 <script setup>
-    //set session cookie
-    if (process.server) {
-        const key = useCookie("quickpaste_auth", {
-            httpOnly: true
-        });
-        if (key.value === undefined) {
-            const res = await fetch(useRuntimeConfig().authServiceAddress + "/keys/generate");
-            if (res.ok) {
-                const data = await res.json();
-                key.value = data.result;
-            }
+//set session cookie
+if (process.server) {
+    const key = useCookie("quickpaste_auth", {
+        httpOnly: true
+    });
+    if (key.value === undefined) {
+        console.log(useRuntimeConfig());
+        const res = await fetch(useRuntimeConfig().authServiceAddress + "/keys/generate");
+        if (res.ok) {
+            const data = await res.json();
+            key.value = data.result;
         }
     }
+}
 </script>
